17e9bf6a1ff04f3b46e8fcac7e2c74e866821cfd
0xRamadan <mah64725@gmail.com>
2023-09-20 16:51:59 +0300

    and env file

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: 'cad3d2dd706547a9a950dfb35e3630ff'
+        key: process.env.API_KEY
     },
 });
