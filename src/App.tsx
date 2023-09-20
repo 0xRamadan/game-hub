73d5c64f2472bf99a3f98afc2dcc7bd5c0584238
0xRamadan <mah64725@gmail.com>
2023-09-20 17:00:25 +0300

    configure env file correctly

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ function App() {
 
   const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery)
 
+  console.log(import.meta.env.VITE_API_KEY);
   return (
     <Grid
       templateAreas={{
