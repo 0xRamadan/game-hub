7c3826946630de59b967c886459f2862f94b2538
0xRamadan <mah64725@gmail.com>
2023-09-22 18:51:41 +0300

    refactoring: remove duplicate interfaces, delete unnecessary useData hook

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,13 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import useData, { FetchResponse } from "./useData";
+import { FetchResponse } from "../services/api-client";
 import apiClient from "../services/api-client";
-
-export interface Platform {
-    id: number;
-    name: string;
-    slug: string;
-}
+import { Platform } from "./usePlatforms";
 
 export interface Game {
     id: number;
@@ -32,18 +27,3 @@ const useGames = (gameQuery: GameQuery) => useQuery<FetchResponse<Game>, Error>(
 });
 
 export default useGames;
-
-
-// const useGames = (gameQuery: GameQuery) =>
-//     useData<Game>(
-//         '/games',
-//         {
-//             params: {
-//                 genres: gameQuery.genre?.id,
-//                 platforms: gameQuery.platform?.id,
-//                 ordering: gameQuery.sortOrder,
-//                 search: gameQuery.searchText
-//             }
-//         },
-//         [gameQuery]
-//     );
