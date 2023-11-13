d150736d77031f46d104b8819b00f3f500215134
0xRamadan <mah64725@gmail.com>
2023-11-13 12:40:04 +0200

    Fetch a game

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -5,11 +5,16 @@ interface Props {
     children: React.ReactNode
 }
 
-const GameCardContainer = ({children}: Props) => {
-  return (
-    <Box borderRadius={10} overflow='hidden'>
-        {children}
-    </Box>
+const GameCardContainer = ({ children }: Props) => {
+    return (
+        <Box
+            _hover={{
+                transform: 'scale(1.03)',
+                transition: 'transform .15s ease-in'
+            }}
+            borderRadius={10} overflow='hidden'>
+            {children}
+        </Box>
     )
 }
 
